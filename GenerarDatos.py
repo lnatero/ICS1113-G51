9f8bde3549c4309c35ce8dc2a922f58cb66e7c0b
Lucas Natero <64765306+lnatero@users.noreply.github.com>
2024-06-03 15:08:35 -0400

    init

---
+++ GenerarDatos.py
@@ -0,0 +1,217 @@
+import pandas as pd
+import random
+from DatosSolarAtlas import GeneracionSolar_ZonasNaranjas,GeneracionSolar_ZonasAmarillas,GeneracionSolar_ZonasRojas,GeneracionSolar_ZonasVerdes,DatosDemanda
+#La ubicaciones disponibles U y K estan ordenadas de norte a sur.
+#Modelaremos de Arica hasta Puerto Montt, porque, la zona mas austral de chile es muy compleja de modelar, debido a que es muy dificil encontrar un patron en los datos en esa zona.
+
+def ProduccionSolar(u, t): 
+    
+    ZonaRoja= u_max * 3 // 5  #Ubicaciones de Arica a Petorca (Primero 3/5 de Chile)
+    ZonaNaranja =  ZonaRoja + (u_max -  ZonaRoja) // 2 #Ubicaciones de Petorca hasta los Angeles
+    
+    #El resto de Ubicaciones es de los Angeles a Puerto Montt
+    
+    # Asigna diferentes valores de G_iut según la ubicacion
+    if u <=  ZonaRoja:
+        return int(GeneracionSolar_ZonasRojas[t]) * 1000
+    
+    elif  ZonaRoja < u <= ZonaNaranja:
+        return int(GeneracionSolar_ZonasNaranjas[t]) * 1000
+    
+    else:
+        return int(GeneracionSolar_ZonasAmarillas[t]) * 1000
+   
+def ProduccionEolica(u, t):
+    # Aquí puedes definir tu propia fórmula para calcular G_iut de energia eolica
+    a = 55000
+    b = 299000
+    g = random.randint(a, b) #Le puse un random pa q le hiciera competencian a la solar
+    return g
+
+def ProduccionMaritima(u, t):
+    # Aquí puedes definir tu propia fórmula para calcular G_iut de energia Maritima
+    return 100 + u * 10 + t * 2  
+
+def ProduccionHidroelectrica(u, t):
+    # Aquí puedes definir tu propia fórmula para calcular G_iut de energia Hidroelectrica
+    return 100 + u * 10 + t * 2  
+
+
+
+
+
+def Calculo_CostoContruccion(i,u): #Calcular costo segun ubicacion y tipo de energia
+    if i == 1: #Si es solar cuesta tanto contruir la central
+        return 100
+    elif i == 2: #Si es eolica cuesta tanto construir la central
+        return 100
+    
+def Calculo_CostoProduccion(i,u): #Calcular costo Produccion segun ubicacion y tipo de energia
+    if i == 1: #Si es solar cuesta tanto producirla  (y/o mantenerla)
+        return 0.1
+    elif i == 2: #Si es eolica cuesta tanto prducirla
+        return 0.3
+
+
+def  AsignacionDemanda(k_max,k,t):#Calcular costo Produccion segun ubicacion y tipo de energia 
+    return (DatosDemanda[t]//k_max)*700 #Demanda mensual dividia en partes iguales por cada subestacion, Se multiplica las 700 horas que tiene un mes.
+
+
+def  AsignacionDisponibilidad(u,i): #Calcular costo Produccion segun ubicacion y tipo de energia
+    return 1
+    
+    
+
+def crear_tablaGeneracion(i_range, u_max, t_max):
+    datos = {
+        "i": [],
+        "u": [],
+        "t": [],
+        "G_iut": []
+    }
+    for i in i_range:
+        for u in range(u_max + 1):
+            for t in range(1, t_max + 1):
+                if i == 1: 
+                    G_iut = int(ProduccionSolar(u, t))
+                elif i == 2:
+                    G_iut = ProduccionEolica(u, t)
+                elif i == 3:
+                    G_iut = ProduccionHidroelectrica(u, t)
+                elif i == 3:
+                    G_iut = ProduccionMaritima(u, t)
+
+                datos["i"].append(i)
+                datos["u"].append(u)
+                datos["t"].append(t)
+                datos["G_iut"].append(G_iut)
+
+    df = pd.DataFrame(datos)
+    return df
+
+def crear_tablaCostosContruccion(i_range, u_max):
+    datos = {
+        "i": [],
+        "u": [],
+        "C_iu": []
+    }
+    for i in i_range:
+        for u in range(u_max + 1):
+            C_iu = Calculo_CostoContruccion(i, u)
+            datos["i"].append(i)
+            datos["u"].append(u)
+            datos["C_iu"].append(C_iu)
+
+    df = pd.DataFrame(datos)
+    return df
+
+def crear_tablaCostoProduccion(i_range, u_max):
+    datos = {
+        "i": [],
+        "u": [],
+        "F_iu": []
+    }
+    for i in i_range:
+        for u in range(u_max + 1):
+            F_iu = Calculo_CostoProduccion(i, u)
+            datos["i"].append(i)
+            datos["u"].append(u)
+            datos["F_iu"].append(F_iu)
+
+    df = pd.DataFrame(datos)
+    return df
+
+def crear_tablaCostoProduccion(i_range, u_max):
+    datos = {
+        "i": [],
+        "u": [],
+        "F_iu": []
+    }
+    for i in i_range:
+        for u in range(u_max + 1):
+            F_iu = Calculo_CostoProduccion(i, u)
+            datos["i"].append(i)
+            datos["u"].append(u)
+            datos["F_iu"].append(F_iu)
+
+    df = pd.DataFrame(datos)
+    return df
+
+def crear_tablaDemanda(k_max, t_max):
+    datos = {
+        "k": [],
+        "t": [],
+        "D_kt": []
+    }
+    for t in range(1, t_max + 1):
+        for k in range(k_max + 1):
+            D_kt = AsignacionDemanda(k_max,k, t)
+            datos["k"].append(k)
+            datos["t"].append(t)
+            datos["D_kt"].append(D_kt)
+
+    df = pd.DataFrame(datos)
+    return df
+
+def crear_tablaDisponibilidad(i_range, u_max):
+    datos = {
+        "u": [],
+        "i": [],
+        "A_ui": []
+    }
+    for i in i_range:
+        for u in range(u_max + 1):
+            A_ui = AsignacionDisponibilidad(i_range, u_max)
+            datos["u"].append(u)
+            datos["i"].append(i)
+            datos["A_ui"].append(A_ui)
+
+    df = pd.DataFrame(datos)
+    return df
+
+
+def generar_tablaDistancias(u_max, k_max):
+    datos = {
+        "u": [],
+        "k": [],
+        "Dist_uk": []
+    }
+   
+
+    # Calcular las distancias entre cada par de puntos en U y K
+    for u in range(u_max+1):
+        for k in range(k_max+1):
+            
+            distancia = int(abs(u-((u_max/k_max)*k)))*10
+            if distancia == 0:
+                distancia+=10
+            datos["u"].append(u)
+            datos["k"].append(k)
+            datos["Dist_uk"].append(distancia)
+    
+    df = pd.DataFrame(datos)
+    return df
+
+
+# Especifica los rangos y valores máximos
+i_range = [1,2] #Energias (1=solar, 2=eolica, 3 = Hidroelectricas)
+k_max = 10 #Cantidad de subestaciones
+u_max = 50 #Cantidad de ubicaciones
+t_max = 12# Meses
+
+# Crear la tabla
+tablaGen = crear_tablaGeneracion(i_range, u_max, t_max)
+tablaCostos_C = crear_tablaCostosContruccion(i_range, u_max)
+tablaCostos_P = crear_tablaCostoProduccion(i_range, u_max)
+tablaDemanda = crear_tablaDemanda(k_max, t_max)
+tablaDisponibilidad = crear_tablaDisponibilidad(i_range, u_max)
+tablaDistancias = generar_tablaDistancias(u_max, k_max)
+# Mostrar
+
+# Guardar la tabla en un archivo CSV
+tablaGen.to_csv("tablaGeneracion.csv", index=False)
+tablaCostos_C.to_csv("tablaCostosContruccion.csv", index=False)
+tablaCostos_P.to_csv("tablaCostosProduccion.csv", index=False)
+tablaDemanda.to_csv("tablaDemanda.csv", index = False)
+tablaDisponibilidad.to_csv("tablaDisponibilidad.csv", index = False)
+tablaDistancias.to_csv("tablaDistancias.csv", index = False)
---
+++ ModeloProyecto.py
@@ -0,0 +1,121 @@
+from gurobipy import Model, GRB, quicksum
+import pandas as pd
+import csv
+
+generacion_df = pd.read_csv('tablaGeneracion.csv')
+demanda_df = pd.read_csv('tablaDemanda.csv')
+costo_df = pd.read_csv('tablaCostosContruccion.csv')
+disponibilidad_df = pd.read_csv('tablaDisponibilidad.csv')
+distancia_df = pd.read_csv('tablaDistancias.csv')
+costo_produccion = pd.read_csv('tablaCostosProduccion.csv')
+
+G = {(row['i'], row['u'], row['t']): row['G_iut'] for idx, row in generacion_df.iterrows()}
+D = {(row['k'], row['t']): row['D_kt'] for idx, row in demanda_df.iterrows()}
+C = {(row['i'], row['u']): row['C_iu'] for idx, row in costo_df.iterrows()}
+A = {(row['u'], row['i']): row['A_ui'] for idx, row in disponibilidad_df.iterrows()}
+Dist = {(row['u'], row['k']): row['Dist_uk'] for idx, row in distancia_df.iterrows()}
+F = {(row['i'], row['u']): row['F_iu'] for idx, row in costo_produccion.iterrows()}
+
+Distmax = 130
+M = 1000000000
+C_transporte = 100
+print("Generación Promedio Esperada (G):", G)
+print("Demanda Energética Promedio (D):", D)
+print("Costo de Construcción (C):", C)
+print("Disponibilidad para Construcción (A):", A)
+
+
+# Conjuntos
+U = list(disponibilidad_df['u'].unique())  # Ubicaciones
+I = list(disponibilidad_df['i'].unique())  # Tipos de plantas: 1 = Solar, 2 = Eólica
+K = list(demanda_df['k'].unique())         # Subestaciones
+T = list(generacion_df['t'].unique())      # Meses
+
+
+m = Model("Minimize_Construction_Costs")
+
+# Variables de decisión
+y = m.addVars(I, U, vtype=GRB.BINARY, name="y")
+z = m.addVars(U, K, vtype=GRB.BINARY, name="z")
+X = m.addVars(U, K, T, vtype=GRB.CONTINUOUS, name="X")
+
+# Función objetivo: Minimizar el costo de construcción
+m.setObjective(quicksum(C[i, u] * y[i, u] for i in I for u in U) +
+               quicksum(C_transporte * Dist[u, k] * z[u, k] for u in U for k in K) +
+               quicksum(X[u, k, t] * F[i, u] for i in I for u in U for k in K for t in T),
+               GRB.MINIMIZE)
+
+# Restricción 1: Satisfacer la demanda total en cada mes con X
+for t in T:
+    for k in K:
+        m.addConstr(quicksum(X[u, k, t] for u in U) >= D[k, t], name=f"Demand_satisfaction_{k}_{t}")
+        
+
+# Restricción: Una planta no puede repartir más que su generación por mes (con variable X)
+for u in U:
+    for t in T:
+        m.addConstr(quicksum(G[i, u, t] * y[i, u] for i in I)>= quicksum(X[u, k, t] for k in K), 
+                    name=f"Plant_generation_limit_{u}_{t}")
+
+        
+# Restricción 2: Disponibilidad de ubicaciones
+for i in I:
+    for u in U:
+        m.addConstr(y[i, u] <= A[u, i], name=f"Availability_{i}_{u}")
+
+# Restricción 3: Relacionar Z con y
+for u in U:
+    for k in K:
+        m.addConstr(quicksum(y[i, u] for i in I) - z[u, k] >= 0, name=f"z_relation_{u}_{k}")
+
+# Restricción 4: Condición para variable Z
+
+
+for u in U:
+    for k in K:
+        m.addConstr(Distmax + M * (1 - z[u, k]) >= Dist[u, k], name=f"Condition_z_{u}_{k}")
+
+
+# Restricción 5: Un punto donde no se construye ninguna planta no puede satisfacer puntos de demanda
+for u in U:
+    for k in K:
+        m.addConstr(quicksum(y[i, u] for i in I) >= z[u, k], name=f"No_plant_demand_{u}_{k}")
+
+
+#Retsriccion 7         
+for u in U:
+    m.addConstr(quicksum(y[i, u] for i in I) <= 1, name=f"One_plant_location_{u}")
+    
+#Restriccion para Z y X
+for u in U:
+    for k in K:
+        for t in T:
+            m.addConstr(X[u, k, t] <= z[u, k] * M, name=f"No_energy_disconnected_{u}_{k}_{t}")
+            
+#No negatividad
+for u in U:
+    for k in K:
+        for t in T:
+            m.addConstr(X[u, k, t] >= 0, name=f"Non_negativity_X_{u}_{k}_{t}")
+
+m.addConstr(quicksum(z[u, k] for u in U for k in K) >= 1, name="At_least_one_z")
+# Optimizar modelo
+m.optimize()
+
+
+
+# Imprimir costo total
+print(f"Costo anual total : {m.ObjVal}")
+
+
+for t in T:
+    filename = f'mes{t}.csv'
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['Ubicación', 'Tipo Energía', 'Subestación', 'Energía (MWh)'])
+        for u in U:
+            for k in K:
+                if X[u, k, t].X > 0:
+                    for i in I:
+                        if y[i, u].X > 0.5:  # Verificar si se construyó una planta en la ubicación u
+                            writer.writerow([u, i, k, X[u, k, t].X])
